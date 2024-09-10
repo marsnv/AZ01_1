4c7ee1094616026cf831cd89325df2fe104fed8b
marsnv <work_apm@mail.ru>
2024-09-10 18:43:50 +0300

    New ver.AZ01_1.001

--- main.py
+++ main.py
@@ -3,4 +3,10 @@ df = pd.read_csv('DataAnalyst.csv')
 print(df.head())
 print(df.info())
 print(df.describe())
-print(df['Location'])
+
+print('*************************')
+df = pd.read_csv('dz.csv')
+df.fillna({'City':'Неизвестно', 'Salary':0}, inplace=True)
+group = df.groupby('City')['Salary'].mean()
+print(group)
+
